253cae7770eb17c08e0b8f0a10db0c1fb6cbda6d
Inês Soares <ines.soares.22102003@gmail.com>
2023-05-29 19:40:41 +0100

    Another attempt of the second function, now for the RWG

--- src/TAH.cpp
+++ src/TAH.cpp
@@ -42,3 +42,37 @@ vector<int> tah_tw(const vector<Node>& nodes){
 
     return tour;
 }
+//-----------------------RWG-----------------------------//
+vector<int> tah_rwg(const vector<Vertex>& vertices) {
+    int numVertices = vertices.size();
+    vector<bool> visited(numVertices, false);
+    vector<int> tour;
+    int current = 0;
+
+    visited[current] = true;
+    tour.push_back(current);
+
+    while (tour.size() < numVertices) {
+        int nearestNeighbor = -1;
+        double minDistance = numeric_limits<double>::max();
+
+        const Vertex& currentVertex = vertices[current];
+        const std::vector<Edge*>& adjEdges = currentVertex.getAdj();
+
+        for (Edge* edge : adjEdges) {
+            Vertex* neighbor = edge->getDest();
+            if (!neighbor->isVisited() && edge->getDist() < minDistance) {
+                nearestNeighbor = neighbor->getID();
+                minDistance = edge->getDist();
+            }
+        }
+
+        visited[nearestNeighbor] = true;
+        tour.push_back(nearestNeighbor);
+        current = nearestNeighbor;
+    }
+
+    tour.push_back(0);
+
+    return tour;
+}
